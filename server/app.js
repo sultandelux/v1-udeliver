222c2948d77105e97d81c6b3e7e3abbbc017dc3a
sultan <sultanwebmobile@gmail.com>
2019-12-21 16:57:50 +0600

    dist

--- server/app.js
+++ server/app.js
@@ -9,11 +9,11 @@ const path = require('path')
 //routes middleware
 
 //here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use('/', serveStatic(path.join(__dirname, '../dist')))
 
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
-	res.sendFile(path.join(__dirname, '/dist/index.html'))
+	res.sendFile(path.join(__dirname, '../dist/index.html'))
 })
 
 //here we are configuring dist to serve app files
